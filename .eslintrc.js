c5870f9f647e46b8b7b7f8fdcfc865d7446d5274
Norman Carcamo <normancarcamo@gmail.com>
2021-03-08 11:53:27 -0600

    Added jest and jest lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,30 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'jest', 'plugin:jest/style'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'airbnb-typescript',
     'prettier',
+    'plugin:jest/recommended',
   ],
   parserOptions: {
     project: './tsconfig.json',
+  },
+  rules: {
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error',
+  },
+  env: {
+    'jest/globals': true,
+  },
+  settings: {
+    jest: {
+      version: 26,
+    },
   },
 };
