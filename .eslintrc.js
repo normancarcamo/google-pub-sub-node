66839f39074d1d1277106a7516d4a58e17c577ee
Norman Carcamo <normancarcamo@gmail.com>
2021-03-08 12:02:32 -0600

    added eslint-plugin-node

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,11 @@ module.exports = {
     'airbnb-typescript',
     'prettier',
     'plugin:jest/recommended',
+    'plugin:node/recommended',
   ],
   parserOptions: {
     project: './tsconfig.json',
+    ecmaVersion: 2020,
   },
   rules: {
     'jest/no-disabled-tests': 'warn',
@@ -18,6 +20,15 @@ module.exports = {
     'jest/no-identical-title': 'error',
     'jest/prefer-to-have-length': 'warn',
     'jest/valid-expect': 'error',
+    'node/exports-style': ['error', 'module.exports'],
+    'node/file-extension-in-import': ['error', 'always'],
+    'node/prefer-global/buffer': ['error', 'always'],
+    'node/prefer-global/console': ['error', 'always'],
+    'node/prefer-global/process': ['error', 'always'],
+    'node/prefer-global/url-search-params': ['error', 'always'],
+    'node/prefer-global/url': ['error', 'always'],
+    'node/prefer-promises/dns': 'error',
+    'node/prefer-promises/fs': 'error',
   },
   env: {
     'jest/globals': true,
